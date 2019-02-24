47181691a76320cb70b53cee25d0db85e43c1689
Etienne Boespflug <numenor.sdz@gmail.com>
2019-02-24 18:32:47 +0100

    Started base.

--- src/faqbuilder/process/process.go
+++ src/faqbuilder/process/process.go
@@ -14,6 +14,11 @@ func Process(faq *model.FAQ, params *util.Params) {
     fmt.Println("Processing FAQ...")
     fmt.Println(" - root_folder:  " + params.RootFolder)
     fmt.Println(" - build_folder: " + params.BuildFolder)
+
+    // Compute local path of each object : Question / Section
+    // Replace all Q with real URL
+    // Replace all faq: with real URL
+    // Notice all local link (to be copied later)
 }
 
 func PathToRootFolder(path string, faq *model.FAQ) string {
